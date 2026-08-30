8a822c9abd93adcba329b746dbf83020f081cdf0
ManhVN <manhvanngo16052006@gmail.com>
2026-08-30 17:25:43 +0700

    Add model for user and subscription

--- config/env.js
+++ config/env.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-undef */
 import { config } from "dotenv";
 
-// Nạp file biến môi trường (đường dẫn path phụ thuộc vào tên file bạn dùng)
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-// Export các biến ra để file mongoDB.js có thể import
 export const { DB_URI, NODE_ENV, PORT } = process.env;
