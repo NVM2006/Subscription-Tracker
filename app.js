bcddf4b6d8cb624e2a7b1e97307fc8cc56abec60
ManhVN <manhvanngo16052006@gmail.com>
2026-08-31 21:49:56 +0700

    Add Arcjet secure api

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ import subscriptionRouter from "./routes/subscriptionRouter.js";
 import userRouter from "./routes/userRouter.js";
 
 import errorMiddleware from "./middleware/errorMiddleware.js";
+import arcjetMiddleware from "./middleware/arcjetMiddleware.js";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extends: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
